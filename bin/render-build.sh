24ce0025cf92b1194be64b3202f167bafdced14a
yuta02gg <yuta02gg@outlook.jp>
2026-05-22 00:13:05 +0900

    Renderビルド時にBunをインストール

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,11 @@
 
 set -o errexit
 
+curl -fsSL https://bun.sh/install | bash
+
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
